6b526acb5ff4d23509ef0813b8d5e21367f1b0f8
Chase Martinez <chasemartinez22@gmail.com>
2022-09-08 16:57:53 -0500

    Feat: Bot response to text channels

--- src/main/java/bot/spaz/SpazBot.java
+++ src/main/java/bot/spaz/SpazBot.java
@@ -1,17 +1,33 @@
 package bot.spaz;
 
+import ignored.Token;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
 import javax.security.auth.login.LoginException;
 
-public class DiscordBot {
+public class SpazBot extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException {
 
-        String token = new Tokens().getBotToken();
-        System.out.println(token);
+        String token = new Token().getToken();
 
-//        JDA bot = JDABuilder.createDefault(token)
-//                .setActivity(Activity.playing("with your mom"))
-//                .build();
+        JDA bot = JDABuilder.createDefault(token)
+                .setActivity(Activity.playing("with your mom"))
+                .addEventListeners(new SpazBot())
+                .build();
     }
 
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        System.out.println("testing");
+        String messageSent = event.getMessage().getContentStripped();
+        if (event.getChannel().toString().equals("spaz-bot-testing-ground")) {
+            event.getChannel().sendMessage("You said: " + messageSent).queue();
+        }
+    }
 }
