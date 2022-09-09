d5c653fb9e4b0dffd9b3b8675c00e78d5c954b2c
Chase Martinez <chasemartinez22@gmail.com>
2022-09-09 13:43:07 -0500

    Changed name of SpazBotListeners and placed code in it's own class

--- src/main/java/bot/spaz/SpazBot.java
+++ src/main/java/bot/spaz/SpazBot.java
@@ -4,13 +4,9 @@ import ignored.Token;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
-
 import javax.security.auth.login.LoginException;
 
-public class SpazBot extends ListenerAdapter {
+public class SpazBot {
 
     public static void main(String[] args) throws LoginException {
 
@@ -18,16 +14,7 @@ public class SpazBot extends ListenerAdapter {
 
         JDA bot = JDABuilder.createDefault(token)
                 .setActivity(Activity.playing("with your mom"))
-                .addEventListeners(new SpazBot())
+                .addEventListeners(new SpazBotListeners())
                 .build();
-    }
-
-    @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        String messageSent = event.getMessage().getContentRaw();
-
-        if (!event.getAuthor().isBot()) {
-            event.getChannel().sendMessage("You said: " + messageSent).queue();
-        }
     }
 }
