f3cbaff0ebcbea504a58be1fb92f47042e1f470d
Chase Martinez <chasemartinez22@gmail.com>
2022-09-19 17:15:50 -0500

    Updating with current structure of VoiceFileSaver object

--- src/main/java/bot/spaz/VoiceFileSaver.java
+++ src/main/java/bot/spaz/VoiceFileSaver.java
@@ -13,19 +13,33 @@ public class VoiceFileSaver {
     }
 
     public void updateStream(UserAudio userAudio) {
+        byte[] newAudio = userAudio.getAudioData(1);
+        User user = userAudio.getUser();
+        newAudio = convertFromStereoToMono(newAudio);
+
         // If user key exists, the new byte[] is added to the existing byte[]
-        if (usersAudioData.containsKey(userAudio.getUser())) {
-            byte[] existingAudioData = usersAudioData.get(userAudio.getUser());
-            //TODO insert method that combines byte[] data
-            addNewByteArrayTo(existingAudioData, userAudio.getAudioData(1));
-            usersAudioData.put(userAudio.getUser(), existingAudioData);
+        if (usersAudioData.containsKey(user)) {
+            byte[] existingAudioData = usersAudioData.get(user);
+            usersAudioData.put(user, createMetaAudio(existingAudioData, newAudio));
         } else {
+
             // If user key does not exist, creates new hashmap with new user key and byte[]
             usersAudioData.put(userAudio.getUser(), userAudio.getAudioData(1));
         }
     }
 
-    public void addNewByteArrayTo(byte[] existingByteArray, byte[] newByteArray) {
+    public byte[] convertFromStereoToMono(byte[] newAudio){
+        //TODO method needs logic for converting stereo to mono, update return statement
+        return newAudio;
+    }
 
+    public byte[] createMetaAudio(byte[] existingAudio, byte[] newAudio) {
+        //TODO method needs logic for combining byte[]s, update return statement
+        byte[] combinedArray = existingAudio;
+        return combinedArray;
+    }
+
+    public void saveAudio(){
+        //TODO method needs logic for saving or overwriting audio file, based on user ID, must be WAVE format
     }
 }
