0804b9e518c2d94469b56243b078ae1c6238a648
Chase Martinez <chasemartinez22@gmail.com>
2023-01-05 13:15:21 -0600

    Removed portion of code for voice to text, not working, will work on after a few more features are developed

--- src/main/java/bot/spaz/SpazTextListener.java
+++ src/main/java/bot/spaz/SpazTextListener.java
@@ -37,67 +37,7 @@ public class SpazTextListener extends ListenerAdapter {
             voiceListener.run(event);
         }
         if (message.getContentRaw().equalsIgnoreCase("-play")) {
-            final int BUFFER_SIZE = 128000;
-            File soundFile = null;
-            AudioInputStream audioStream = null;
-            AudioFormat audioFormat;
-            SourceDataLine sourceLine = null;
-            String fileName = "/Users/chasemartinez/IdeaProjects/java-discord-bot/src/main/resources/soundclips/BRUH.mp3";
-            try {
-                try {
-                    soundFile = new File(fileName);
-                } catch (Exception e){
-                    e.printStackTrace();
-                    System.out.println("Error creating soundFile");
-                    System.exit(1);
-                }
-
-                try {
-                    audioStream = AudioSystem.getAudioInputStream(soundFile);
-                } catch (Exception e){
-                    e.printStackTrace();
-                    System.out.println("Error creating audioStream");
-                    System.exit(1);
-                }
-
-                audioFormat = audioStream.getFormat();
-
-                DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-                try {
-                    sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-                    sourceLine.open();
-                } catch (LineUnavailableException e){
-                    e.printStackTrace();
-                    System.out.println("Error with sourceLine creation - Line Unavailable Exception");
-                    System.exit(1);
-                } catch (Exception e){
-                    e.printStackTrace();
-                    System.out.println("Error with sourceLine creation - general error");
-                    System.exit(1);
-                }
-
-                sourceLine.start();
-
-                int nBytesRead = 0;
-                byte[] abData = new byte[BUFFER_SIZE];
-                while (nBytesRead != -1) {
-                    try {
-                        nBytesRead = audioStream.read(abData, 0, abData.length);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (nBytesRead >= 0) {
-                        @SuppressWarnings("unused")
-                        int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-                    }
-                }
-
-                sourceLine.drain();
-                sourceLine.close();
-
-            } catch (Exception e) {
-                System.out.println("Error playing sound clip (text listener class)");
-            }
+            // music player commands
         }
     }
 }
