3a06a3687af06293d0da1b3d51cc8fd4cdb2cb2a
Chase Martinez <chasemartinez22@gmail.com>
2023-01-06 19:13:20 -0600

    Feat: -play function in CmdPlay now working, a skip function must be added to stop player otherwise kicking the bot and rejoining, the player continues to play audio until finished

--- src/main/java/bot/spaz/commands/CmdPlay.java
+++ src/main/java/bot/spaz/commands/CmdPlay.java
@@ -3,14 +3,47 @@ package bot.spaz.commands;
 import bot.spaz.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class CmdPlay extends ListenerAdapter{
 
-    public CmdPlay() {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        VoiceChannel userVoiceChannel;
+        TextChannel userTextChannel = event.getChannel().asTextChannel();
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        String link = event.getMessage().getContentRaw().substring(5).trim();
+
+        if (message[0].equalsIgnoreCase("-play")) {
+            try {
+                userVoiceChannel = (VoiceChannel) event.getMessage().getMember().getVoiceState().getChannel();
+                if (userVoiceChannel == null) {
+                    event.getChannel().sendMessage("You must be in a voice channel to use music player.").queue();
+                    return;
+                }
+                if (userVoiceChannel.getGuild().getSelfMember().getVoiceState().getChannel() != userVoiceChannel) {
+                    event.getChannel().sendMessage("I'm already in a different channel BRUH.").queue();
+                    return;
+                }
+
+                if(!isURL(link)){
+                    play(link, userVoiceChannel, userTextChannel);
+                }
+                userVoiceChannel.getGuild().getAudioManager().openAudioConnection(userVoiceChannel);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void play(String link, VoiceChannel voiceChannel, TextChannel textChannel) {
@@ -20,11 +53,9 @@ public class CmdPlay extends ListenerAdapter{
         if (link.trim().equals("")) {
             textChannel.sendMessage("Empty request.").queue();
         } else {
-
             if (!isURL(link)) {
                 link = "ytsearch:" + link + " audio";
             }
-            textChannel.sendMessage("(TEST) You want to listen to **" + link + "**").queue();
             // play music
             PlayerManager.getINSTANCE().loadAndPlay(textChannel, link);
         }
