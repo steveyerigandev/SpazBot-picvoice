ae6dc1ce4b810ddc25c101f3ba0b2e56cb6d3beb
Chase Martinez <chasemartinez22@gmail.com>
2023-01-09 16:57:07 -0600

    Feat: Skip command added as CmdSkip

--- src/main/java/bot/spaz/commands/CmdSkip.java
+++ src/main/java/bot/spaz/commands/CmdSkip.java
@@ -1,5 +1,8 @@
 package bot.spaz.commands;
 
+import bot.spaz.lavaplayer.PlayerManager;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -13,10 +16,28 @@ public class CmdSkip extends ListenerAdapter {
             return;
         }
 
+        VoiceChannel userVoiceChannel;
+        TextChannel userTextChannel = event.getChannel().asTextChannel();
         String[] message = event.getMessage().getContentRaw().split(" ");
-        String song = "TEST SONG TITLE";
-        if(message[0].equalsIgnoreCase("-skip")){
-            event.getChannel().sendMessage("(TEST) Skipping **" + song + "**.").queue();
+        String link = event.getMessage().getContentRaw().substring(5).trim();
+
+        if (message[0].equalsIgnoreCase("-skip")) {
+            try {
+                userVoiceChannel = (VoiceChannel) event.getMessage().getMember().getVoiceState().getChannel();
+                if (userVoiceChannel == null) {
+                    event.getChannel().sendMessage("You must be in a voice channel to use music player.").queue();
+                    return;
+                }
+                if (userVoiceChannel.getGuild().getSelfMember().getVoiceState().getChannel() != userVoiceChannel) {
+                    event.getChannel().sendMessage("I'm in a different channel BRUH.").queue();
+                    return;
+                }
+
+                PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).scheduler.nextTrack();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
