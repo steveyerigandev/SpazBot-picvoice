23eee65841518dd81b0ad1122ca3a3015abe8ce4
Chase Martinez <chasemartinez22@gmail.com>
2023-01-09 17:00:07 -0600

    Refactor: Removed unused code and unnecessary code from CmdSkip

--- src/main/java/bot/spaz/commands/CmdSkip.java
+++ src/main/java/bot/spaz/commands/CmdSkip.java
@@ -1,7 +1,6 @@
 package bot.spaz.commands;
 
 import bot.spaz.lavaplayer.PlayerManager;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -17,15 +16,12 @@ public class CmdSkip extends ListenerAdapter {
         }
 
         VoiceChannel userVoiceChannel;
-        TextChannel userTextChannel = event.getChannel().asTextChannel();
         String[] message = event.getMessage().getContentRaw().split(" ");
-        String link = event.getMessage().getContentRaw().substring(5).trim();
 
         if (message[0].equalsIgnoreCase("-skip")) {
             try {
                 userVoiceChannel = (VoiceChannel) event.getMessage().getMember().getVoiceState().getChannel();
                 if (userVoiceChannel == null) {
-                    event.getChannel().sendMessage("You must be in a voice channel to use music player.").queue();
                     return;
                 }
                 if (userVoiceChannel.getGuild().getSelfMember().getVoiceState().getChannel() != userVoiceChannel) {
