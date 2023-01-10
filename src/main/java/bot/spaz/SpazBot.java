0b344ec071f971cb3bfe5d907d39cc950db7344a
Chase Martinez <chasemartinez22@gmail.com>
2023-01-10 14:41:22 -0600

    Refactor: Changed name of JDA token

--- src/main/java/bot/spaz/SpazBot.java
+++ src/main/java/bot/spaz/SpazBot.java
@@ -4,7 +4,7 @@ import bot.spaz.commands.CmdJoin;
 import bot.spaz.commands.CmdLeave;
 import bot.spaz.commands.CmdPlay;
 import bot.spaz.commands.CmdSkip;
-import ignored.Token;
+import ignored.JDAToken;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -18,7 +18,7 @@ public class SpazBot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
 
         // Create bot
-        JDABuilder.createDefault(new Token().getToken())
+        JDABuilder.createDefault(new JDAToken().getToken())
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_VOICE_STATES)
                 .setActivity(Activity.playing("the dangerous game of figuring out how to code a bot"))  // sets default bot activity
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
