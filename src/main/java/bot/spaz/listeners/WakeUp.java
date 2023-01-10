c0bf6772311430b40eafa01258139c48d22c94b4
Chase Martinez <chasemartinez22@gmail.com>
2023-01-10 15:24:29 -0600

    Working on adding Wake Up word instance

---
+++ src/main/java/bot/spaz/listeners/WakeUp.java
@@ -0,0 +1,15 @@
+package bot.spaz.listeners;
+
+import ai.picovoice.porcupine.Porcupine;
+import ai.picovoice.porcupine.PorcupineException;
+import ignored.PicoToken;
+
+public class WakeUp {
+    Porcupine porcupine = new Porcupine.Builder()
+            .setAccessKey(PicoToken.getToken())
+            .setBuiltInKeywords(new Porcupine.BuiltInKeyword[]{Porcupine.BuiltInKeyword.PICOVOICE, Porcupine.BuiltInKeyword.BUMBLEBEE})
+            .build();
+
+    public WakeUp() throws PorcupineException {
+    }
+}
